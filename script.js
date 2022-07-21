12627d497f770eb03749769f936c4021071be57e
Rory <rory.james2021@gmail.com>
2022-07-21 16:11:24 -0500

    add function with operator and operand inputs

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 }
+
+// Operate function
+
+function operate (operator, operandOne, operandTwo) {
+    switch (operator) {
+        case '+':
+            return add(operandOne, operandTwo);
+            break;
+        
+        case '-':
+            return subtract(operandOne, operandTwo);
+        
+        case '*':
+            return multiply(operandOne, operandTwo);
+        
+        case '/':
+            return divide(operandOne, operandTwo);
+        
+        default:
+            return "ERROR: invalid operator";
+    }
+}
