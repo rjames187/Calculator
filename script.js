42395607455790de6993e94c73aaa1cc2b595c8f
Rory <rory.james2021@gmail.com>
2022-07-22 15:58:59 -0500

    add functionality to operator buttons

--- script.js
+++ script.js
@@ -45,3 +45,52 @@ function operate (operator, operandOne, operandTwo) {
             return "ERROR: invalid operator";
     }
 }
+
+// interactivity
+
+let displayNumbers = "";
+const display = document.getElementById("display");
+let operandStack = [0];
+let curOperator = "+";
+
+// add functionality to number buttons
+const numButtons = document.querySelectorAll('.digit');
+for (let btn of numButtons) {
+    btn.addEventListener('click', () => {
+        // division by 0 not allowed
+        if (curOperator === "/" && btn.id === "0") {
+            return;
+        }
+        displayNumbers += btn.id;
+        display.textContent = displayNumbers;
+    });
+}
+
+function evaluate () {
+    operandStack.push(displayNumbers);
+    let result = operate(curOperator, operandStack[0], operandStack[1]);
+    operandStack.shift();
+    operandStack.shift();
+    operandStack.unshift(result);
+
+    display.textContent = result;
+    operandStack = [result];
+    displayNumbers = "";
+}
+
+// add functionality to operator buttons
+const opButtons = document.querySelectorAll('.operator');
+for (let op of opButtons) {
+    op.addEventListener('click', () => {
+        if (!(op.id === "=")) {
+            evaluate();
+            
+            curOperator = op.id;
+        } else {
+            evaluate();
+            
+            operandStack = [0]
+            curOperator = '+';
+        }
+    });
+}
